03ba2614bc1cf3da4517a42c1ee8d5b6ff3d4f72
Rooney22 <znaevas@mail.ru>
2023-11-11 00:50:34 +0600

    test-deploy

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import speech_recognition as sr
+from voice_recignizing_button import voice_recignizing_button
+import vosk
+def main():
+    model = vosk.Model("vosk-model-small-ru-0.22")
+    st.title("Голосовой ассистент")
+
+    st.markdown(
+        """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    voice_recignizing_button(model)
+
+if __name__ == "__main__":
+    main()
---
+++ voice_recignizing_button.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import speech_recognition as sr
+from voice_recognize import vosk_voice_recignize
+
+def voice_recignizing_button(model):
+    st.markdown(
+         """
+        <style>
+        .stButton{
+            position: fixed;
+            right: 20px;
+            bottom: 20px; 
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    if st.button("",key='voice_button'):
+      try:
+       text = vosk_voice_recignize(model)
+       st.write(f"Вы сказали: {text}")
+      except sr.UnknownValueError:
+            st.write("Кажется, не удалось распознать речь")
+      except sr.RequestError:
+                st.write("Кажется, возникла проблема с доступом к сервисам распознавания речи")
